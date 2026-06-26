7852ffd1597445303afd162c2cf4906ec8737903
larissapereiras-del <larissa.pereiras@mercadolivre.com>
2026-06-25 23:59:12 -0300

    Create app.js

---
+++ css/js/app.js
@@ -0,0 +1,9 @@
+/* =======================================
+   HUB OPERACIONAL
+   ======================================= */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Hub Operacional iniciado 🚀");
+
+});
